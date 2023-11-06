aec63ef9c2b09c8c8cef613c4e8e947adc16cd54
Prakhar Buliya <prakharbuliya02@gmail.com>
2023-11-07 00:39:41 +0530

    Created the basic UI

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let cols = 26;
 
 let addressColCont = document.querySelector(".address-col-cont");
 let addressRowCont = document.querySelector(".address-row-cont");
+let cellsCont = document.querySelector(".cells-cont");
+let addressBar = document.querySelector(".address-bar");
 
 for (let i = 0; i < rows; i++) {
   let addresscol = document.createElement("div");
@@ -16,4 +18,26 @@ for (let i = 0; i < cols; i++) {
   addressrow.setAttribute("class", "address-row");
   addressrow.innerText = String.fromCharCode(65 + i);
   addressRowCont.appendChild(addressrow);
+}
+
+for (let i = 0; i < rows; i++) {
+  let rowCont = document.createElement("div");
+  rowCont.setAttribute("class", "row-cont");
+  for (let j = 0; j < cols; j++) {
+    let cell = document.createElement("div");
+    cell.setAttribute("class", "cell");
+    cell.setAttribute("contenteditable", "true");
+    rowCont.appendChild(cell);
+    addListnerForAddressBarDisplay(cell, i, j);
+  }
+  cellsCont.appendChild(rowCont);
+}
+
+function addListnerForAddressBarDisplay(cell, i, j) {
+  cell.addEventListener("click", (e) => {
+    let rowID = i + 1;
+    let colID = String.fromCharCode(65 + j);
+
+    addressBar.value = `${colID}${rowID}`;
+  });
 }
